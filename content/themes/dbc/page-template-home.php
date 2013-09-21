00e6cb34ae379f460e006d1bf5c1688b15330be4
Patrick Daly <patrick@developdaly.com>
2013-09-21 09:03:48 -0500

    Moved The City button styles to home.css, moved the logo into the theme and called with environtment relative URL

--- content/themes/dbc/page-template-home.php
+++ content/themes/dbc/page-template-home.php
@@ -15,29 +15,29 @@ get_header(); ?>
 	<div id="content" role="main">
 
 		<?php do_atomic( 'open_content' ); // dbc_open_content ?>
-		
+
 		<div class="hfeed">
 
 			<?php get_template_part( 'slider-home' ); // loads slider-home.php ?>
-	
+
 			<div id="mydbc">
 				<a class="big-button city-button" href="https://secure.accessacs.com/access/login_guest.aspx?sn=92231">
-					<div class="inner"><img src="http://dentonbible.org/../wp-content/uploads/city-button-logo.png"><div><span class="league-gothic">Find a Group<br>Find Church Events<br>Get Connected</span></div></div></a>
+					<div class="inner"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/city-button-logo.png"><div><span class="league-gothic">Find a Group<br>Find Church Events<br>Get Connected</span></div></div></a>
 				<a class="big-button green" href="<?php echo site_url(); ?>/admin/mydbc-life-faq/"><div class="inner"><span class="league-gothic center">Connect to</span><img class="right" src="http://dentonbible.org/../wp-content/uploads/mydbc-icon.png"></div></a>
 			</div><!-- #mydbc -->
-		
+
 			<?php get_template_part( 'latest-message' ); // loads latest-message.php ?>
-			
+
 			<div class="clear"></div>
-			
+
 			<?php get_sidebar( 'home-features' ); // Loads sidebar-home-features.php ?>
-						
+
 		</div><!-- .hfeed -->
-			
+
 		<?php do_atomic( 'close_content' ); // dbc_close_content ?>
 
 	</div><!-- #content -->
-		
+
 	<?php get_sidebar( 'home' ); ?>
 
 	<?php do_atomic( 'after_content' ); // dbc_after_content ?>
